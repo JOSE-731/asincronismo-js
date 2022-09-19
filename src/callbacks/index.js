5fb875e08e42ce6b4e14036d9e279bc58c4d189c
Jose David <jose7312000@gmail.com>
2022-09-19 11:16:08 -0500

    Ejecutando callbacks

--- src/callbacks/index.js
+++ src/callbacks/index.js
@@ -17,9 +17,11 @@ function div(num1, num2) {
     return num1 / num2;
 }
 
-function calc(num1, num2, callback) {
-    return callback(num1, num2);
+function calc(num1, num2, callbackSum) {
+    return callbackSum(num1, num2);
 };
+
+console.log(calc(2, 517, sum))
 
 
 //Callback: una funcion que recibe otra funcion para ser ejecutada, ejemplo de una forma que nos trae por defecto js
